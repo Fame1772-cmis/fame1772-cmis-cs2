90b3e78da7277f0d938fface659d979660d8230f
cmis <cmis@cmis-hs07.(none)>
2016-01-27 11:11:19 +0700

    Commit made changes to files

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+print "hello world"
+print "I am Fame"
+print "Computer Scienceeeeeeeeeeeeee"
+print True
+print False
+print 54320458973450981
+print 8756
+print -2345
+print 8.58758
+print "Good, fox"
+print ("Fame, Cold", 14, "Snow")
+print (13243412314212342, True,  "Can, Dog")
