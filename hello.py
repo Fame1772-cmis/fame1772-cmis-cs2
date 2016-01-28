7df21d1d59a15f05e85665c5027485e83a4501c5
cmis <cmis@cmis-hs07.(none)>
2016-01-28 14:33:46 +0700

    Commit made changes to file

--- hello.py
+++ hello.py
@@ -11,3 +11,25 @@ print 8.58758
 print "Good, fox"
 print ("Fame, Cold", 14, "Snow")
 print (13243412314212342, True,  "Can, Dog")
+Shape = "Heart"
+Color = "Pink"
+Age = 14
+Animal= "Panda"
+Eyecolor= "Blue"
+Species= "WhiteTiger"
+print Shape
+print Color
+print Age
+print Animal
+print Eyecolor
+print Species
+Appleprices = 20
+Mangoprices = 50
+Orangeprices= Appleprices + Mangoprices
+print Orangeprices
+Sisterage= 18
+Myage= 14
+Totalage=Sisterage - Myage
+print Totalage
+
+
