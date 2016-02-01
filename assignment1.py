1ca15d90ef152788964f90dfc5042c5d2f68e91b
cmis <cmis@cmis-hs07.(none)>
2016-02-01 09:55:31 +0700

    Commit made changes to file

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+myname = "Fame"
+print myname
+myageinyears = 14.8
+print myageinyears
+myheightinmeters = 1.66
+print myheightinmeters
+onesquarelength= 3
+print onesquarelength
+rectanglelength = 30
+print rectanglelength
+rectangleheight= 5    
+print rectangleheight 
+myageinmonths= myageinyears * 12
+print myageinmonths
+yearsleft= 74 - myageinyears
+print yearsleft
+myheightinfeet= myheightinmeters * 3.28084
+print myheightinfeet
+thaigirlvsme = myheightinmeters - 1.59
+averagethaiheight= 1.59
+print thaigirlvsme
+areaofsquare = onesquarelength * onesquarelength
+print areaofsquare
+volumeofcube = onesquarelength * onesquarelength * onesquarelength
+print volumeofcube
