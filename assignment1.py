74cfe818f83435c9522230aa5b25a29c742d6d2c
cmis <cmis@cmis-hs07.(none)>
2016-02-02 11:55:54 +0700

    Commit made changes to file

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,14 @@
-myname = "Fame"
+myname = "Fame" # tells my name
 print myname
-myageinyears = 14.8
+myageinyears = 14.8 # tells my age
 print myageinyears
-myheightinmeters = 1.66
+myheightinmeters = 1.66 #tells my height
 print myheightinmeters
-onesquarelength= 3
+onesquarelength= 3 #tells the length of 1 side of a square
 print onesquarelength
-rectanglelength = 30
+rectanglelength = 4
 print rectanglelength
-rectangleheight= 5    
+rectangleheight= 3  
 print rectangleheight 
 myageinmonths= myageinyears * 12
 print myageinmonths
@@ -21,5 +21,17 @@ averagethaiheight= 1.59
 print thaigirlvsme
 areaofsquare = onesquarelength * onesquarelength
 print areaofsquare
-volumeofcube = onesquarelength * onesquarelength * onesquarelength
+rectanglewidth = 12
+volumeofcube = rectanglelength * rectangleheight * rectanglewidth
 print volumeofcube
+halfvolumeofcube = volumeofcube / 2
+print halfvolumeofcube
+areaofrectangle = rectanglelength * rectanglewidth
+print areaofrectangle
+oneninth = 1.0 / 9.0
+oneninthareaofrectangle = areaofrectangle * oneninth
+print oneninthareaofrectangle
+print "Hello. My name is " + myname + "." " My height is " + str (myheightinmeters) + " meters." " I am " + str (myageinyears) + " years old." " I only have " + str (yearsleft) + " years left to live." " I am " + str (thaigirlvsme) + " meters taller than the height of the average Thai girl."
+print "Hello. My square has an area of", areaofsquare, "." " My rectangle has an area of", areaofrectangle, "." " My cube has a volume of", volumeofcube, "." " Half the volume of the cube is ", halfvolumeofcube, "." " One ninth the area of my rectangle is", oneninthareaofrectangle, "."
+smileyface= ";)"
+print smileyface * 10000
