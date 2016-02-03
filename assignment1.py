7f94102cb56fdef3f271d6ee773156cfda8596fc
cmis <cmis@cmis-hs07.(none)>
2016-02-03 14:36:12 +0700

    Commit made changes to file

--- assignment1.py
+++ assignment1.py
@@ -1,37 +1,21 @@
 myname = "Fame" # tells my name
-print myname
 myageinyears = 14.8 # tells my age
-print myageinyears
 myheightinmeters = 1.66 #tells my height
-print myheightinmeters
 onesquarelength= 3 #tells the length of 1 side of a square
-print onesquarelength
-rectanglelength = 4
-print rectanglelength
-rectangleheight= 3  
-print rectangleheight 
-myageinmonths= myageinyears * 12
-print myageinmonths
-yearsleft= 74 - myageinyears
-print yearsleft
-myheightinfeet= myheightinmeters * 3.28084
-print myheightinfeet
-thaigirlvsme = myheightinmeters - 1.59
-averagethaiheight= 1.59
-print thaigirlvsme
-areaofsquare = onesquarelength * onesquarelength
-print areaofsquare
-rectanglewidth = 12
-volumeofcube = rectanglelength * rectangleheight * rectanglewidth
-print volumeofcube
-halfvolumeofcube = volumeofcube / 2
-print halfvolumeofcube
-areaofrectangle = rectanglelength * rectanglewidth
-print areaofrectangle
-oneninth = 1.0 / 9.0
-oneninthareaofrectangle = areaofrectangle * oneninth
-print oneninthareaofrectangle
+rectanglelength = 4 #tells the length of the rectangle
+rectangleheight= 3  # tells the height of the rectangle
+myageinmonths= myageinyears * 12 #tells my age in months
+yearsleft= 74 - myageinyears #tells how many years that I have left to live
+myheightinfeet= myheightinmeters * 3.28084 #tells my height in feet
+thaigirlvsme = myheightinmeters - 1.59 #tells the height difference between me and the average Thai girl
+averagethaiheight= 1.59 #tells the average height of a Thai girl
+areaofsquare = onesquarelength * onesquarelength #Tells the average of the square
+rectanglewidth = 12 # tells the width of the rectangle
+volumeofcube = rectanglelength * rectangleheight * rectanglewidth #Tells the volume of the cube
+halfvolumeofcube = volumeofcube / 2 #tells half the volume of the cube
+areaofrectangle = rectanglelength * rectanglewidth #Tells the area of the rectangle
+oneninth = 1.0 / 9.0 #Tells one divided by nine
+oneninthareaofrectangle = areaofrectangle * oneninth #Tells one ninth of the rectangle
 print "Hello. My name is " + myname + "." " My height is " + str (myheightinmeters) + " meters." " I am " + str (myageinyears) + " years old." " I only have " + str (yearsleft) + " years left to live." " I am " + str (thaigirlvsme) + " meters taller than the height of the average Thai girl."
 print "Hello. My square has an area of", areaofsquare, "." " My rectangle has an area of", areaofrectangle, "." " My cube has a volume of", volumeofcube, "." " Half the volume of the cube is ", halfvolumeofcube, "." " One ninth the area of my rectangle is", oneninthareaofrectangle, "."
-smileyface= ";)"
-print smileyface * 10000
+smileyface= ";)" #Tells what a smiley face looks like
