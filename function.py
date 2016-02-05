6aac222733ec9505bf2adf83454e08daed43f32c
cmis <cmis@cmis-hs07.(none)>
2016-02-05 09:54:33 +0700

    Commit changes to file

---
+++ function.py
@@ -0,0 +1,7 @@
+def greet_three():
+        print "Hello there!"
+        print "I haven't seen you here before!"
+        print "I would like to know you better."
+        print "My name is Fame."
+
+greet_three()
