9f3835ed172cce5b97e592c35a40c1221cdcfe09
cmis <cmis@cmis-hs07.(none)>
2016-02-08 11:55:23 +0700

    Commit made changes to file

--- function.py
+++ function.py
@@ -1,7 +1,2 @@
-def greet_three():
-        print "Hello there!"
-        print "I haven't seen you here before!"
-        print "I would like to know you better."
-        print "My name is Fame."
-
-greet_three()
+def add (a, b):
+	return (
