f60e5f851035b055cba27aabb58862104105408c
cmis <cmis@cmis-hs07.(none)>
2016-02-09 14:06:48 +0700

    Commit made changes to file

--- assignment1.py
+++ assignment1.py
@@ -19,3 +19,5 @@ oneninthareaofrectangle = areaofrectangle * oneninth #Tells one ninth of the rec
 print "Hello. My name is " + myname + "." " My height is " + str (myheightinmeters) + " meters." " I am " + str (myageinyears) + " years old." " I only have " + str (yearsleft) + " years left to live." " I am " + str (thaigirlvsme) + " meters taller than the height of the average Thai girl."
 print "Hello. My square has an area of", areaofsquare, "." " My rectangle has an area of", areaofrectangle, "." " My cube has a volume of", volumeofcube, "." " Half the volume of the cube is ", halfvolumeofcube, "." " One ninth the area of my rectangle is", oneninthareaofrectangle, "."
 smileyface= ";)" #Tells what a smiley face looks like
+print smileyface * 10000
+
--- function.py
+++ function.py
@@ -1,2 +1,34 @@
 def add (a, b):
-	return (
+	return a + b
+
+c= add (3, 4)
+print c
+
+def sub (d, e):
+	return d - e
+
+f= sub (5, 3)
+print f
+def mul (g, h):
+	return g * h
+
+i=mul (4, 4)
+print i
+
+def div (j, k):
+	return (j / k)
+l=div (2.0, 3.0)
+print l
+
+def hours_from_seconds (a):
+	return a / 3600
+print hours_from_seconds (86400)
+
+def circle_area (a):
+	return 3.14159265359 * (a**2)
+print circle_area (5)
+
+def sphere_volume (a):
+	return 1.33333333333 * 3.14159265359 * (a**3)
+print sphere_volume (5)
+
