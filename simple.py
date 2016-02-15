6fb7907cbd4b65e6acd2b74e9656e3f2fdc4d3bd
cmis <cmis@cmis-hs07.(none)>
2016-02-15 14:35:27 +0700

    Commit made changes to file

---
+++ simple.py
@@ -0,0 +1,10 @@
+s=raw_input ("Type a number>>> ")
+t=raw_input ("Type a number>>> ")
+print s+t
+print int(s)+int(t)
+
+x=raw_input("Type a number: ")
+y=raw_input ("Type another: ")
+z= int(x) +int(y)
+print str(x) + "+" + str(y) + "="+str(z)
+
