7fa76f197f804e1f7fd87e5f898d918ddcc8f421
Fame1772-cmis <fame1772@cmis.ac.th>
2016-02-22 19:39:58 +0700

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,6 +1,18 @@
-import math 
-def pythagorean_theorem (a, b, c,):
-	return math.pow ((a) + (b) + (c))
-a= pythagorean_theorem (5, 7, 9)
-print a
-
+def mul (x, y)
+	return x * y
+	
+def output (name, x)
+	left_to_live="""
+Hey {}!
+You have {} years left to live
+	return left_to_live
+	
+def main ():
+	name=
+	z=
+	y=
+	
+	x=mul (int(z), int(y))
+	left_to_live=output(name, x)
+	print ""
+main ()
