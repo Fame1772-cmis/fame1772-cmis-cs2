baec147e57116efea5cd4127ef277b91ba469bcc
cmis <cmis@cmis-hs07.(none)>
2016-03-02 09:53:14 +0700

    Commit made changes to file

---
+++ cs2quiz1.py
@@ -0,0 +1,76 @@
+#Part 1: Terminology (15 points)
+#1 1pt) What is the symbol "=" used for?
+# The assignment operator is used to put a value into a variable.
+#
+#
+#2 3pts) Write a technical definition for 'function'
+#The technical definition for function is a named sequence of statements that performs a computation.
+#
+#
+#3 1pt) What does the keyword "return" do?
+#The keyword return spits out an output. The function takes an arguement and spits ouf the results.
+#
+#
+#4 5pts) We know 5 basic data types. Write the name for each one and provide two
+#   examples of each below
+#	1: tuple; ex; ("fame", 23, 2.5) ("test", 9, 3)
+#	2: boolean ex; True, False
+#	3: string ex; "fame" "cats"
+#	4: float ex; 2.5, 5.5
+#	5: integer ex; 4, 10
+#
+#5 2pts) What is the difference between a "function definition" and a 
+#        "function call"?
+#A function definition defines the function, the name and the sequence of statements must be specified. Function call is a statement that executes a function, it consists of the function name and an argument list.  
+#
+#
+#
+#6 3pts) What are the 3 phases that every computer program has? What happens in
+#        each of them
+#	1:input, the critical functions used in the program is defined
+#	2:calculating and processing, the information given is taken and used to do computations
+#	3:output, from the processing, there will be an output.
+#
+#Part 2: Programming (25 points)
+#Write a program that asks the user for the areas of 3 circles.
+#It should then calculate the diameter of each and the sum of the diameters 
+#of the 3 circles.
+#Finally, it should produce output like this:
+
+#Circle  Diameter
+#c1      ...
+#c2      ...
+#c3      ...
+#TOTALS  ...
+
+# Hint: Radius is the square root of the area divided by pi
+import math
+def radius (C1, C2, C3):
+    return diameter1
+    return diameter2
+    return diameter3
+
+ def output (diameter1, diameter2, diameter3):
+    diameter="""
+    Circle
+    C1
+    C2
+    C3
+    Diameter
+    {}
+    {}
+    {}
+    {}
+   """. format(diameter1, diameter2, diameter3)
+    return diameter
+
+def main (): 
+    C1= raw_input("Area of C1:")
+    C2= raw_input("Area of C2:")
+    C3= raw_input("Area of C3:")
+    diameter1=(int(2)*(math.sqrt(C1/pi)))
+    diameter2=(int(2)*(math.sqrt(C2/pi)))
+    diameter3=(int(2)*(math.sqrt(C3/pi)))
+    diameter=output(diameter1, diameter2, diameter3)
+    print diameter    
+main()
