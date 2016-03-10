e4f5ebe8ba1e614fb56c949b0baffea604596075
cmis <cmis@cmis-hs07.(none)>
2016-03-10 14:35:58 +0700

    Commit made changes to file

---
+++ oneguess.py
@@ -0,0 +1,14 @@
+def ():
+	return
+def output ():
+	out = """
+""". format ()
+	return out
+def main ():
+	mininum= raw_input ("What is the minimum number?:") 
+	maximum= raw_input ("What is the maximum number?:") 
+	thinking= raw_input ("I'm thinking of a number:")
+	answer= raw_input ("What do you think it is?:")
+	target= raw_input ("The target was:")
+	guess= raw_input ("Your guess was:")
+That's under by 2.
