83e037d061449f700ca560ab20d40889e71a4084
cmis <cmis@cmis-hs07.(none)>
2016-03-16 11:55:17 +0700

    commit made changes to file

--- oneguess.py
+++ oneguess.py
@@ -1,26 +1,50 @@
 import random
-def number(mini, maxi):
-	return 
+def number(minimum, maximum):
+	return target
 
-def output (target, guess, wrong):
+def bigguess (target, guess, result):
 	out = """
 	
 The target was {}
 Your guess was {}
 That's under by {}
-""". format (target, guess, wrong)
+""". format (target, guess, result)
+	return out
+def smallguess (target, guess, result):
+	out = """
+	
+The target was {}
+Your guess was {}
+That's under by {}
+""". format (target, guess, result)
+	return out
+def rightguess (target, guess, result):
+	out = """
+	
+The target was {}
+Your guess was {}
+That's under by {}
+""". format (target, guess, result)
 	return out
 
 def main ():
 	mininum= raw_input ("What is the minimum number?:") 
-	maximum= raw_input ("What is the maximum number?:") 
-	print "I'm thinking of a number from int(minimum) to int(maximum)"
-	answer= raw_input ("What do you think it is?:")
-	target = random.randint((), ())
-	if answer==target:
+	maximum= raw_input ("What is the maximum number?:") 	
+	think= raw_input ("I'm thinking of a number from" + str(minimum) "to" + str(maximum) + "./n" "What do you think it is?:")
+	if target > str(abs(guess)):
+	guess1=smallguess(target, guess, result)
+	print guess1
+	elif target < str(abs(guess)):
+	guess2= bigguess (target, guess, result)
+	print guess2
+	elif target == str(abs(guess)):
+	guess3= rightguess (target, guess, result)
+	elif target== str(abs(guess)):
+	print guess3
+
+		
 	
 	out=output(target, guess, wrong)
-	print output
+	print out
 	
-main ()
-mini, 
+main () 
