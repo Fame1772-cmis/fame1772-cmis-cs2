a5a32cc560a748289c5125bbe4f913613b112626
cmis <cmis@cmis-hs04.(none)>
2016-03-22 09:55:33 +0700

    Made changes

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+#The computer will take your grades and see what your occupation possibilities are
+
+import random
+def main ():
+	Freetime= int(raw_input ("What do you like to do during your freetime? Sports, Reading & Writing, Singing & Dancing, Art, Music & Instruments, Photography & Filming >>>"))
+	Sports= random.randint(Basketball, Football, Volleyball)
+	if Freetime == Sports:
+		OccupationSports = random.randint(1, 3))
+	return OccupationSports
+	if OccupationSports== 1
+	print """
+Congratulations you are baller
+""" . format (target, guess, result1)
+
+main ()
+			
--- structure.py
+++ structure.py
@@ -1,23 +1,23 @@
-def possiblelove (z, y, x):
-	return z * y * x
+def possiblelove (Userlen, Crushlen, Favnum):
+	return Userlen ** Crushlen / Favnum
 	
-def output (p1, p2, love):
+def output (User, Crush, love):
 	love_posibility="""
 Hey {}!
 Your love posibility with {} is {}%
 Good luck with LOVE! <3
 Please pay 100 baht before you exit!
-""". format(p1, p2, love)
+""". format(User, Crush, love)
 	return love_posibility
 
 def main ():
-	p1= raw_input("What is your name?:")
-	z= len(p1)
-	p2=raw_input("What is your crush's name?:")
-	y=len(p2)
-	x=raw_input("What is your favorite number?:")
+	User= raw_input("What is your name?:")
+	Userlen= len(User)
+	Crush=raw_input("What is your crush's name?:")
+	Crushlen=len(Crush)
+	Favnum=raw_input("What is your favorite number?:")
 
-	love=possiblelove (int(z), int(y), int(x))
-	love_possibility=output(p1, p2, love)
+	love=possiblelove (int(Userlen), int(Crushlen), int(Favnum))
+	love_possibility=output(User, Crush, love)
 	print love_possibility
 main ()
