70d2aeab21a0d1ee531669cc07e877d7a86ac602
cmis <cmis@cmis-hs04.(none)>
2016-03-29 09:54:33 +0700

    Commit made changes to files

--- conditionals.py
+++ conditionals.py
@@ -1,33 +1,150 @@
-#You are in your father's house. Your father just remarried woman who had 2 daughters. Your father just passed away, the cause is unknown. Immediately after the death of your father, your stepmother and stepsister started treating you like slaves. One day, a letter to the prince's ball at the palace arrives at your doorstep. 
+def output (User):
+	Workage="""
+Hey {}!
+You will begin working at {}!
+""". format (User)
+	return Workage
 
-import random
+def age (User):
+	Userlen= len(User)
+	Currentage= float(raw_input ("How old are you?"))
+	Finishschool= float(raw_input ("At what age do you think that you will finish school?"))
+	Thinkstartwork= float(raw_input ("At what age do you want to start working?"))
+	return ((Userlen ** Currentage) / (Finishschool ** Thinkstartwork))
+
 def main ():
-	print "You are now Cinderella. Your father just remarried a woman who had 2 daughters. Your father just passed away, the cause is unknown. Immediately after the death of your father, your stepmother and stepsister started treating you like slaves. One day, when you step outside your house, you see a letter at your doorstep."
-	letter = raw_input ("""
-What will you do with that letter?
-a. Burn it up, you don't want your stepmother and stepsisters to see this!
-b. Immediately hand the letter to your stepmother and stepsisters, who knows what will happen if you don't
-c. Pretend you never saw it, and go back inside.
-d. Open the letter, you can't help but be curious!
+	User= raw_input("What is your name?")
+	kj = age (User)
+	asfd = output (User)
+	
+main ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#The computer will take determine the age at which you start you occupation, and what your occupation might be.
+import random
+def ageforwork (Workage):
+	return (Userlen ** Currentage / Finishschool % Thinkstartwork)
+
+def output (User, Workage):
+	Workage="""
+Hey {}!
+You will begin working at {}!
+""". format (User, Workage)
+	return Workage
+
+def age ():
+	User= raw_input("""What is your name?""")
+	Userlen= len(User)
+	Currentage= raw_input ("""How old are you?""")
+	Finishschool= raw_input ("""At what age do you think that you will finish school?""")
+	Thinkstartwork= raw_input ("""At what age do you want to start working?""") 
+	return age
+
+def Ageatwork ():
+	Working=ageforwork (int(Userlen), int(Currentage), int(Finishschool), int(Thinkstartwork))
+	Workage=output(User, Workage)
+	print workage
+	return Ageatwork
+
+def Occupationoptions ():
+	Freetime= raw_input ("""
+What do you like to do during your freetime? 
+a. Sports
+b. Reading & Writing
+c. Music
+d. Photography & Filming 
 Answer (Type: a, b, c, or d)
 """)
-	if letter == "a":
-		print "You bring the letter to the fireplace and toss the letter. You watch it burn up into flames. The next day, when you come back home, you see your stepmother and her 2 daughters reading the exact same letter that you saw yesterday."
-		ball = raw_input("""
-How do you react to this scene?
-a. Ask to read the letter, if everyone is looking so excited, then it must be good news!
-b. Start walking back to your room, you don't care about the letter
-Answer (Type: a or b)
-""")		if ball == "a": 
-			print "You go up and asks about the letter. Your stepmother and stepsisters talk about the ball. Then they look at each other and laugh at you. Apparently, they think that maids should not be invited.
+	if Freetime == "a":
+		Sports= raw_input ("""
+What sports do you like?
+a. Basketball
+b. Volleyball
+c. Soccer
+d. Badminton
+Answer (Type: a, b, c, or d)
+""")
+		if Sports == "a":
+			print "Congratulations, you were born to be a basketball player!"
+		elif Sports == "b": 
+			print "Congratulations, you were born to be a volleyball player!"
+		elif Sports == "c":
+			print "Congratulations, you were born to be a soccer player!"
 		else:
-			print "Your stepmother calls you and tells you about the ball. Then her and her daughters look at each other and laugh at you. Apparently, they think that maids should not be invited. 
-	elif letter == "b":
-		print "You go back inside the house and give the letter to your stepmother. Her daughters are waiting in anticipation."
-	elif letter == "c":
-		print "You rush back inside the house, and go back to your room." 
-	else:
-		print "You open the letter and read its contents. The prince is asking all the girls in town to attend the ball at the palace."
+			print "Congratulations, you were born to be a badminton player!"
 
+	elif Freetime == "b":
+		Study= raw_input ("""
+What do genre do you like to read/write?
+a. Fantasy
+b. Psychology
+c. Business
+d. Crime
+Answer (Type: a, b, c, or d)
+""")
+		if Study == "a":
+			print "Congratulations, you were born to be a writer!"
+		elif Study == "b":
+ 			print "Congratulatinos, you were born to be a doctor!"
+		elif Study == "c":
+			print "Congratulations, you were born to be a businessman/businesswoman!"
+		else:
+			print "Congratulations, you were born to be a detective!"
+
+	elif Freetime == "c":
+		Music = raw_input ("""
+What do you like to do?
+a. Sing
+b. Dance
+c. Compose Music
+d. Play Instruments
+Answer (Type: a, b, c, or d)
+""")
+		if Music == "a":
+			print "Congratulations, you were born to be a singer!"
+		elif Music == "b":
+			print "Congratulations, you were born to be a dancer!"
+		elif Music == "c":
+			print "Congratulations, you were born to be a composer!"
+		else:
+			print "Congratulations, you were born to be a musician!"
+            
+	else: 
+		Photography = raw_input ("""
+What do you like to do?
+a. Photography
+b. Acting
+c. Filming
+Answer (Type: a, b, or c)
+""")
+		if Photography == "a":
+			print "Congratulations, you were born to be a photographer!"
+		elif Photography == "b":
+			print "Congratulations, you were born to be a actor!"
+		else: 
+			print "Congratulations, you were born to be a director!"
+def main ():
+	workingage= ageforwork (Workage)
+	out= output (User, Workage)
+	allages= age()
+	Agework= Ageatwork()
+	Joboptions=Occupationoptions()
+	print out
 main ()
 
