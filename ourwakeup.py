b0e45537b3fdd951986950568a7e8c93ac9fc350
cmis <cmis@cmis-hs04.(none)>
2016-04-18 09:53:58 +0700

    Commit made changes to files

---
+++ ourwakeup.py
@@ -0,0 +1,34 @@
+#This  program will ask you for 5 integer or float values.
+#It will calculate the average of all valus from 0 inclusive to 10 exclusive.
+#It will print out whether the resulting average is even or odd.
+
+def number0 (N0):
+	N0= raw_input ("n0:")
+	if N0 < int(0) or N0 > int(10):
+		print float({}) "is out of range"
+number0 ()
+	
+def number1 (N1):
+	N1= raw_input ("n1:")
+	if N1 < int(0) or N1 > int(10):
+		print float({}) "is out of range"
+number1 ()
+
+def number2 (N2):
+	N2= raw_input ("n2:")
+	if N2 < int(0) or N2 > int(10):
+		print float({}) "is out of range"
+number2 ()
+
+def number3 (N3):	
+	N3= raw_input ("n3:")
+	if N3 < int(0) or N3 > int(10):
+		print float({}) "is out of range"
+number3 ()
+
+def number4 (N4):
+	N4= raw_input ("n4:")
+	if N4 < int(0) or N4 > int(10):
+		print float({}) "is out of range")
+number4 ()
+
