fcda2fcb3d5c19c7ecc32e2f8c95471372f094ad
cmis <cmis@cmis-hs04.(none)>
2016-04-22 09:55:23 +0700

    Commit made changes to file

--- conditionals.py
+++ conditionals.py
@@ -1,29 +1,31 @@
 #The computer will take determine the age at which you start you occupation, and what your occupation might be.
 import random
 def agecalculations(Currentage, Finishschool, Thinkstartwork):
-	return (Currentage * Finishschool) / Thinkstartwork
+	return (Currentage * Finishschool) // Thinkstartwork
 
-def output (User, Userages):
+def quitjob ():
+    randnum = random.randint(0, 99)
+    return quitjob
+    
+def output (User, Workingage, Endjob):
 	Workage="""
 Hey {}!
 You will begin working at {}!
-""". format (User, Userages)
+You will quit this job in {} years!
+""". format (User, Workingage, Endjob)
 	return Workage
-
-def age (User):
-	Currentage= int(raw_input ("How old are you?"))
-	Finishschool= int(raw_input ("At what age do you think that you will finish school?"))
-	Thinkstartwork= int(raw_input ("At what age do you want to start working?"))
-	return (Currentage * Finishschool) / Thinkstartwork
-
+	
 def Occupationoptions ():
 	Freetime= raw_input ("""
 What do you like to do during your freetime? 
 a. Sports
 b. Reading & Writing
 c. Music
-d. Photography & Filming 
-Answer (Type: a, b, c, or d)
+d. Photography
+e Gardening
+f. Filming
+g. Computer
+Answer (Type: a, b, c, d, e, f or g)
 """)
 	if Freetime == "a":
 		Sports= raw_input ("""
@@ -46,59 +48,89 @@ Answer (Type: a, b, c, or d)
 	elif Freetime == "b":
 		Study= raw_input ("""
 What do genre do you like to read/write?
-a. Fantasy
-b. Psychology
-c. Business
-d. Crime
-Answer (Type: a, b, c, or d)
+a. Fiction
+b. Non-fiction
+Answer (Type: a, or b)
 """)
 		if Study == "a":
 			print "Congratulations, you were born to be a writer!"
-		elif Study == "b":
- 			print "Congratulatinos, you were born to be a doctor!"
-		elif Study == "c":
-			print "Congratulations, you were born to be a businessman/businesswoman!"
 		else:
-			print "Congratulations, you were born to be a detective!"
+ 			print "Congratulatinos, you were born to be a scientist!"
 
 	elif Freetime == "c":
-		Music = raw_input ("""
-What do you like to do?
+		Music= raw_input ("""
+What do you like to do? Choose 2
 a. Sing
 b. Dance
-c. Compose Music
-d. Play Instruments
-Answer (Type: a, b, c, or d)
+c. Play Instruments
+d. Compose music
+Answer (Type: a, b, c, d)
 """)
-		if Music == "a":
-			print "Congratulations, you were born to be a singer!"
-		elif Music == "b":
-			print "Congratulations, you were born to be a dancer!"
-		elif Music == "c":
-			print "Congratulations, you were born to be a composer!"
-		else:
-			print "Congratulations, you were born to be a musician!"
+		first= raw_input ("1:")
+		second= raw_input ("2:")
+		if first and second == "a" and "b":
+		    print "Congratulations, you were born to be both a singer and a dancer!"
+		elif first and second == "a" and "c":
+		    print "Congratulations, you were born to be both a singer and a composer!"
+	    elif first and second == "a" and "c":
+	        print "Congratulations, you were born to be both a singer and a musician!"
+	    elif first and second == "b" and "c":
+	        print  "Congratulations, you were born to be both a dancer and a composer!"
+	    elif first and second == "b" and "d": 
+	        print "Congratulations, you were born to be both a dancer and a musician!"
+        else:
+            print "Congratulations, you were born to be both a composer and a musician!"
             
-	else: 
-		Photography = raw_input ("""
+
+	elif Freetime == "d":
+	        print "Congratulations, you were born to be a photographer!"
+	
+	elif Freetime == "e":
+	        print "Congratulations, you were born to be a gardener!"
+	        
+    elif Freetime == "f":
+	    Filming= raw_input ("""
 What do you like to do?
-a. Photography
-b. Acting
-c. Filming
-Answer (Type: a, b, or c)
+a. Acting
+b. Filming
+Answer (Type: a or b)
 """)
-		if Photography == "a":
-			print "Congratulations, you were born to be a photographer!"
-		elif Photography == "b":
-			print "Congratulations, you were born to be a actor!"
-		else: 
-			print "Congratulations, you were born to be a director!"
+	    if Filming == "a":
+		    print "Congratulations, you were born to be a actor/actress!"
+	    else: 
+		    print "Congratulations, you were born to be a director!"
+	
+	else:
+        Computer= raw_input ("""
+What do you like to do?
+a. Play computer games
+b. Programming
+Answer (Type: a or b)
+""")
+        if Computer == "a" or "b"
+            print "Congratulations, you were born to be a computer game programmer!"
+        if Computer == not "a" and "b" 
+            print "Sorry, but you have to look at other possible options under Computer!"
+def main ():
+	Joboptions=Occupationoptions()
+main ()
 def main ():
 	User= raw_input("What is your name?")
-	Userages = age (User)
-	Startwork = output (User, Userages)
+	Currentage= int(raw_input ("How old are you?"))
+	Finishschool= int(raw_input ("At what age do you think that you will finish college?"))
+	Thinkstartwork= int(raw_input ("At what age do you want to start working?"))
+	if int(Currentage) >= 20:
+        return True
+    elif int(Currentage) <= 19:
+        return False
+		if Currentage == True:
+			print "It's time to think about your future."
+		else:
+			print "You still have some time to think about your future."
+	Workingage = agecalculations (int(Currentage), int(Finishschool), int(Thinkstartwork))
+	Startwork = output (User, Workingage)
+	Endjob = quitjob ()
 	Joboptions=Occupationoptions()
 	print Startwork
 	print output
 main ()
-
--- count.py
+++ count.py
@@ -1,24 +1,44 @@
 def countdown(n):
 	if n<=0:
-		print "Blastoff!"
+		print "Countdown!"
 	else:
 		print n
 		countdown(n-1)
 
 def countup(n):
 	if n==10:
-		print "Ten!"
+		print "Countup!"
 	else:
 		print n
 		countup(n+1)
 
-def count_up_from(n, stop):
-	if n>stop:
-		print "Blastoff!"
+def count_up_from(start, stop):
+	if start>stop:
+		print "Countupfrom!"
 	else:
-		print n
-		count_up_from(n+1, stop)
+		print start
+		count_up_from(start+1, stop)
 count_up_from(-5, 5)
+
+def countdown_from(start, stop):
+	if stop < start:
+		print "Countdownfrom!"
+	else:	
+		print stop
+		countdown_from(start, stop-1)
+
+countdown_from(1, 10)
+
+def adder(sum):
+	print "Running total: 0"
+	nxtnum= raw_input("Next number:")
+	if raw_input=="":	
+		print sum
+	else:
+		runtotal= "Running total: {}"
+		sum= int(runtotal + nxtnum)
+		print runtotal
+adder(sum)
 
 def main():
 	countdown(10)
