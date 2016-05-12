9c8015fb387c5c8875befb6d24063225cfc06feb
cmis <cmis@cmis-hs04.(none)>
2016-05-12 11:57:24 +0700

    Commit made changes to file

--- guessgame.py
+++ guessgame.py
@@ -1,24 +1,37 @@
-def trials (tries, limit):
-	if int(tries) == int(limit):
-		return game()
-	else: 
-		print tries
-		trials(tries-1,limit)
+import random
+def game (target, tries):
+	guess= int(raw_input ("What do you think it is?:"))
+	if tries == 0:
+		print "You're bad at this game"
+		return 0
+	elif target == guess:
+		print "Correct"
+		return 1`
+	elif int(guess) > int(target):
+		print "Too high"
+		return 0
+		game(target, tries-1)
+	elif int(guess) < int(target):
+		print "Too low"
+		return 0
+		game(target, tries-1)
 
-def game(target):
-	if target == guess:
-		return "Correct"
-	else: 
-		if int(guess) > int(target):
-			return "Too high"
-		elif int(guess) < int(target):
-			return "Too low"
-
-def main ():
-	trials(6,0)
-	import random
+def rounds(roundsies, correct, tries):
 	target=random.randint(0, 100)
-	game ()
+	if roundsies == 0:
+		print "Game over"
+	elif roundsies != 0:
+		games=game(target, tries)
+		adding= float(correct) + float(games)
+		print "You have {} of rounds left".format(roundsies)
+		rounds(roundsies-1, adding, tries)
+def main (): 
+	computer= ("I am thinking of a number between 0 and 100")
+	print computer
+	correct =0
+	tries=4
+	roundsies=3
+	game(target, tries)
 main ()
 
 
