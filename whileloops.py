6c3f3f51bbc5671497ee1ab121d20846b13345d1
cmis <cmis@cmis-hs04.(none)>
2016-05-24 09:55:27 +0700

    Commit made changes to file

---
+++ whileloops.py
@@ -0,0 +1,21 @@
+def countdown (x):
+	while x > 0:
+		print x
+		x -= 1
+		if x == 0:
+			print "Countdown"
+
+def counter(x):
+	while x < 0:
+		print x
+		x+=1
+	while x > 0:
+		print x
+		x -= 1
+
+def addup (x):
+	
+def main():
+	countdown (10)
+	counter(-5)
+main()
