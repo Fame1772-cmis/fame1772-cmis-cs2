6bf7a0a78bc71cd5d725f72d13d70f2304d82975
cmis <cmis@cmis-hs04.(none)>
2016-05-26 14:35:12 +0700

    Commit made changes

--- whileloops.py
+++ whileloops.py
@@ -13,9 +13,25 @@ def counter(x):
 		print x
 		x -= 1
 
-def addup (x):
-	
+def countFrom2 (x, y):
+	while x > y:
+		print x
+		x+=1
+	while y <= x:
+		print x
+		x-=1
+
+def sumOfOdds(n):
+	if n < 0:
+		while n % 2 ==1:
+			sum += n
+	else: 
+		while n % 2 ==1:
+			sum += n
+		
 def main():
 	countdown (10)
 	counter(-5)
+	countFrom2 (2, 5)
+	sumOfOdds (10)
 main()
